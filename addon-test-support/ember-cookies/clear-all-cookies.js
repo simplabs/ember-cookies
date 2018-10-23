721a9de3c2d09d87302d0fc39916d568c6b2cb36
Gabor Babicz <gabor.babicz@gmail.com>
2018-10-23 16:39:22 +0200

    Support receiving options for clearAllCookies test helper
    Resolves #190

--- addon-test-support/ember-cookies/clear-all-cookies.js
+++ addon-test-support/ember-cookies/clear-all-cookies.js
@@ -1,9 +1,19 @@
-export default function() {
+import { assert } from '@ember/debug';
+import { merge, assign as emberAssign } from '@ember/polyfills';
+import { isEmpty } from '@ember/utils';
+import { serializeCookie } from 'ember-cookies/utils/serialize-cookie';
+const assign = Object.assign || emberAssign || merge;
+
+export default function(options = {}) {
+  assert('Max-Age option cannot be set when clearing cookies', isEmpty(options.maxAge));
+  options = assign({}, options, {
+    expires: new Date(0)
+  });
+
   let cookies = document.cookie.split(';');
 
   cookies.forEach((cookie) => {
     let cookieName = cookie.split('=')[0];
-
-    document.cookie = `${cookieName}=; expires=${new Date(0).toUTCString()}`;
+    document.cookie = serializeCookie(cookieName, '', options);
   });
 }
--- tests/unit/clear-all-cookies-test.js
+++ tests/unit/clear-all-cookies-test.js
@@ -1,6 +1,6 @@
 /* jshint expr:true */
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { describe, it, afterEach } from 'mocha';
 import clearAllCookies from 'ember-cookies/clear-all-cookies';
 
 function randomString() {
@@ -8,6 +8,12 @@ function randomString() {
 }
 
 describe('clearAllCookies test helper', function() {
+  afterEach(function() {
+    for (let cookie of ['test1', 'test2']) {
+      document.cookie = `${cookie}=whatever; expires=${new Date(0).toUTCString()}; path=/`;
+    }
+  });
+
   it('clears all cookies', function() {
     let value1 = randomString();
     let value2 = randomString();
@@ -16,7 +22,21 @@ describe('clearAllCookies test helper', function() {
 
     clearAllCookies();
 
-    expect(document.cookie).to.not.include(`test1=${value1};`);
-    expect(document.cookie).to.not.include(`test2=${value2};`);
+    expect(document.cookie).to.not.include(`test1=${value1}`);
+    expect(document.cookie).to.not.include(`test2=${value2}`);
+  });
+
+  it('clears all cookies with a specified path', function() {
+    let pathname = window.location.pathname;
+    let path = pathname.substring(0, pathname.lastIndexOf('/'));
+    let value1 = randomString();
+    let value2 = randomString();
+    document.cookie = `test1=${value1};path=${path};`;
+    document.cookie = `test2=${value2};path=/`;
+
+    clearAllCookies({ path });
+
+    expect(document.cookie).to.not.include(`test1=${value1}`);
+    expect(document.cookie).to.include(`test2=${value2}`);
   });
 });
